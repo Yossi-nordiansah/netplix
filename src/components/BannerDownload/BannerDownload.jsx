46ab1eed592681d02ad63abdb8fd682f5c20575c
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-01-23 11:12:43 +0700

    cek deployment

--- src/components/BannerDownload/BannerDownload.jsx
+++ src/components/BannerDownload/BannerDownload.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const BannerDownload = () => {
   return (
-    <div className='py-[110px] border-b-[9px] border-b-zinc-800 text-white bg-black'>
-      <div className='w-[80%] flex mx-auto justify-center items-center gap-16 flex-col sm:flex-row'>
-        <div className='relative w-[600px]'>
-          <video src="src\assets\video\video-tv-0819.mp4" loop autoPlay className='w-[376px] h-64 ml-[70px]'></video>
-          <img src="src\assets\image\tv.png" alt="" className='absolute w-[520px] block -top-14' />
+    <div className='py-[90px] border-b-[9px] border-b-zinc-800 text-white bg-black'>
+      <div className='w-[80%] flex mx-auto justify-cente flex-col sm:flex-row'>
+        <div className='relative w-[50%]'>
+          <img src="src\assets\image\mobile.jpg" alt="" className='-mt-6 mx-auto w-[87%]' />
+          <div className='absolute left-1/2 -translate-x-1/2 border border-zinc-600 '></div>
         </div>
         <div className='w-[50%]'>
-          <h1 className='font-netflix text-[54px] leading-[60px]'>Download acara TV untuk menontonnya secara offline</h1>
-          <p className='font-netflixrg text-[27px] mt-6 leading-[35px]'>
+          <h1 className='font-netflix text-[54px] leading-[65px]'>Download acara TV untuk menontonnya secara offline</h1>
+          <p className='font-netflixrg font-extrabold text-[25px] mt-6 w-[90%] leading-[35px]'>
             Simpan favoritmu dengan mudah agar selalu ada acara TV dan film yang bisa ditonton.
           </p>
         </div>
--- src/components/Bannertv/BannerTv.jsx
+++ src/components/Bannertv/BannerTv.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const BannerTv = () => {
     return (
-        <div className='py-[145px] border-b-[9px] border-b-zinc-800 text-white bg-black'>
-            <div className='w-[80%] flex mx-auto justify-center items-center gap-16 flex-col sm:flex-row'>
-                <div className='w-[48%]'>
+        <div className='py-[175px] border-b-[9px] border-b-zinc-800 text-white bg-black'>
+            <div className='w-[75%] flex mx-auto justify-center gap-9 flex-col sm:flex-row'>
+                <div className='w-[50%]'>
                     <h1 className='font-netflix text-[54px]'>Nikmati di TV-mu</h1>
-                    <p className='font-netflixrg text-[27px]'>
+                    <p className='font-netflixrg text-[27px] leading-[35px] mt-4'>
                         Tonton di Smart TV, Playstation, Xbox, Chromecast, Apple TV, pemutar Blu-ray, dan banyak lagi.
                     </p>
                 </div>
-                <div className='relative w-[600px]'>
-                    <video src="src\assets\video\video-tv-0819.mp4" loop autoPlay className='w-[376px] h-64 ml-[70px]'></video>
-                    <img src="src\assets\image\tv.png" alt="" className='absolute w-[520px] block -top-14'/>
+                <div className='relative w-[50%] '>
+                    <video src="src\assets\video\video-tv-0819.mp4" loop controls autoPlay className='w-[376px] h-64 -mt-7 ml-[65px] absolute'></video>
+                    <img src="src\assets\image\tv.png" alt="" className='absolute w-[520px] -top-20'/>
                 </div>
             </div>
         </div>
