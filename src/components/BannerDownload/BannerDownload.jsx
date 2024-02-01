c7f68a76efba6d163e7afbf885a8d9304bf55a3b
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-02-01 16:17:00 +0700

    repair download section

--- src/components/BannerDownload/BannerDownload.jsx
+++ src/components/BannerDownload/BannerDownload.jsx
@@ -9,17 +9,19 @@ const BannerDownload = () => {
       <div className=' w-[95%] flex mx-auto justify-center flex-col-reverse lg:flex-row'>
         <div className='relative lg:mt-0 mt-12 xl:w-[45%] lg:w-[50%] w-full'>
           <img src={mobileimg} alt="" className='-mt-6 mx-auto lg:w-[87%] w-full' />
-          <div className='absolute left-1/2 sm:bottom-6 bottom-4 bg-black items-center -translate-x-1/2 sm:py-2 py-1 rounded-xl border-[3px] xl:w-[54%] lg:w-[70%] sm:w-[59%] w-[78%] flex lg:justify-center sm:justify-evenly justify-center border-zinc-600 '>
+          <div className='absolute left-1/2 sm:bottom-6 bottom-4 bg-black items-center -translate-x-1/2 sm:py-2 py-1 rounded-xl border-[3px] xl:w-[54%] lg:w-[70%] sm:w-[59%] min-[380px]:w-[78%] w-[80%] max-[380px]:w-full flex justify-between border-zinc-600 px-3'>
+              <div className='flex items-center'>
               <img src={boxshot} alt="" className='xl:w-[63px] lg:w-[17%] sm:w-12 w-9' />
               <div className='sm:ml-5 ml-2 pr-4'>
-                <p className='sm:text-lg lg:text-[16px] leading-5 font-netflix xl:w-[60%] w-full'>Stranger Things</p>
-                <p className='text-[#3366ff]'>Men-download...</p>
+                <p className='sm:text-lg lg:text-[16px] leading-5 font-netflix xl:w-[60%] w-full max-[380px]:text-sm'>Stranger Things</p>
+                <p className='text-[#3366ff] max-[380px]:text-sm'>Men-download...</p>
+              </div>
               </div>
               <img src={downloadAnimate} alt="" className='w-[50px] sm:ml-0 ml-2 h-12'/>
           </div>
         </div>
-        <div className='lg:w-[60%] xl:w-[45%] w-full self-center border'>
-          <h1 className='font-netflix lg:text-[54px] sm:text-[30px] text-[33px] lg:leading-[65px] lg:ml-0 md:mx-auto sm:mx-auto lg:text-left text-center sm:w-[80%] md:w-full sm:leading-[40px] leading-10 border'>Download acara TV untuk menontonnya secara offline</h1>
+        <div className='lg:w-[60%] xl:w-[45%] w-full self-center'>
+          <h1 className='font-netflix lg:text-[54px] sm:text-[30px] text-[33px] lg:leading-[65px] lg:ml-0 md:mx-auto sm:mx-auto lg:text-left text-center sm:w-[80%] md:w-full sm:leading-[40px] leading-10'>Download acara TV untuk menontonnya secara offline</h1>
           <p className='font-netflixrg lg:text-[27px] sm:text-[21px] text-[18px] lg:text-left text-center sm:mt-6 mt-4 lg:w-[90%] w-full sm:leading-[35px]'>
             Simpan favoritmu dengan mudah agar selalu ada acara TV dan film yang bisa ditonton.
           </p>
--- src/components/BannerStreaming/BannerStreaming.jsx
+++ src/components/BannerStreaming/BannerStreaming.jsx
@@ -4,7 +4,7 @@ import devicevideo from "/src/assets/video/video-devices-id.mp4"
  
 const BannerStreaming = () => {
   return (
-    <div className='xl:py-[210px] lg:py-44 sm:pt-16 pt-11 sm:pb-[600px] py-72 border-b-[9px] border-b-zinc-800 text-white bg-black'>
+    <div className='xl:py-[210px] lg:py-44 sm:pt-16 sm:pb-[600px] py-11 border-b-[9px] border-b-zinc-800 text-white bg-black'>
     <div className='xl:w-[75%] w-[90%] flex mx-auto justify-center gap-7 flex-col lg:flex-row'>
         <div className='xl:w-[50%] lg:w-[70%] w-full'>
             <h1 className='font-netflix lg:text-[54px] sm:text-4xl text-[33px] lg:text-left text-center'>Tonton di mana pun</h1>
@@ -13,8 +13,8 @@ const BannerStreaming = () => {
             </p>
         </div>
         <div className='relative lg:w-1/2 lg:mx-0 mx-auto w-[90%] xl:-mt-11 mt-2'>
-            <video src={devicevideo} loop autoPlay muted className='lg:w-[65%] w-[58%] sm:left-1/2 sm:-translate-x-1/2 lg:-mt-7 sm:mt-11 mt-6 sm:ml-0 ml-[68px] absolute'></video>
-            <img src={deviceimg} alt="" className='absolute w-[100%] lg:-top-20'/>
+            <video src={devicevideo} loop autoPlay muted className='lg:w-[65%] w-[58%] sm:left-1/2 sm:-translate-x-1/2 lg:-mt-7 sm:mt-11 min-[380px]:mt-[27px] mt-6 sm:ml-0 left-1/2 -translate-x-1/2 absolute'></video>
+            <img src={deviceimg} alt="" className='relative w-[100%] lg:-top-20'/>
         </div>
     </div>
 </div>
